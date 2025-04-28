8592f2f9b0ccceafa44db701e113d17f7e01de93
Anjelo Go <deorcstudios@gmail.com>
2025-04-27 21:39:27 -0700

    add api/users to routes

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -3,6 +3,8 @@ api = Blueprint("api", __name__)
 
 from app.blueprints.api.listings import listings
 from app.blueprints.api.auth import auth_blueprint
+from app.blueprints.api.users import users
 
 api.register_blueprint(listings) # /api/listings
 api.register_blueprint(auth_blueprint) # /api/auth
+api.register_blueprint(users) # /api/users
